18387a620e8770938bfe7221128d87efbcb06277
Cristian Nasicmento <93889204+Cristian-Nascimento@users.noreply.github.com>
2022-08-18 20:12:22 -0300

    Update logicaDeProgramacao.py

--- Session-two/logicaDeProgramacao.py
+++ Session-two/logicaDeProgramacao.py
@@ -1,13 +1,14 @@
 #aula 2
 print('Hello World')
 nome = 'Cristian'
-print (nome)
+print(nome)
 
 #aula 3
 a = '083'
 b = '955'
 c = '726'
 d = '46'
+
 print(a, b, c, sep=".", end="-")
 print(d)
 
@@ -23,3 +24,12 @@ print(1.69, type(1.69))
 
 #Boolean: é maior de idade? idade > 18
 print(23>18, type(23>18))
+
+#aula 5 variáveis
+nome1 = "Cristian"
+idade = 23
+altura = 1.69
+peso = 80.5
+imc = peso / altura ** altura
+
+print(nome1, 'tem', idade, 'de idade e seu imc é', imc)
