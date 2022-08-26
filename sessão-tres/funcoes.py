f2fcf249ff0391b8a50c4265b28a86086babb407
Cristian Nasicmento <93889204+Cristian-Nascimento@users.noreply.github.com>
2022-08-25 21:01:34 -0300

    Update funcoes.py

--- sessão-tres/funcoes.py
+++ sessão-tres/funcoes.py
@@ -1,8 +1,18 @@
 """
 Funções
 """
-
 def funcao_1(bar):
     print(bar)
 
 funcao_1('Valor que eu querer')
+
+def divisao(n1, n2):
+    if n2 > 0:
+        return n1 / n2
+
+divide = divisao(830,0)
+
+if divide:
+    print(f'o resultado da divisao é: {divide}')
+else:
+    print('Conta invalida')
